382cbe4858795fac35aadbd448c29f235ab088ca
vyshakh <vyshakh0015@gmail.com>
2025-01-27 19:12:43 +0530

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import "../styles/components/home.scss";
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>Welcome to Collaborative Notes App</h1>
+    </div>
+  );
+}
+
+export default Home;
